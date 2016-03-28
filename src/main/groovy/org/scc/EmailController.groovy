292745a55aacdf7f6336798aa8b79a811fded4c8
luist <luistrigueiros@gmail.com>
2016-03-28 13:31:53 +0100

    clean up mailgun api key

--- src/main/groovy/org/scc/EmailController.groovy
+++ src/main/groovy/org/scc/EmailController.groovy
@@ -3,6 +3,7 @@ package org.scc
 import groovyx.net.http.HTTPBuilder
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -13,12 +14,15 @@ class EmailController {
 
     final static Logger logger = LoggerFactory.getLogger(EmailController.class);
 
+    @Value('${name:key-2a3c50e54d7747aec34a27feb1a9e35c}')
+    private String apiKey;
+
     @RequestMapping("/email")
     @ResponseBody
     String sendEmail(@RequestBody Email email) {
         logger.debug("Email request received $email")
         HTTPBuilder http = new HTTPBuilder('https://api.mailgun.net/v3/sandbox1f1c8a6d340244db94f21f52fc273de8.mailgun.org/messages')
-        http.auth.basic 'api', 'key-2a3c50e54d7747aec34a27feb1a9e35c'
+        http.auth.basic 'api', apiKey
         def response = http.post(query: [
                 from:'Mailgun Sandbox <postmaster@sandbox1f1c8a6d340244db94f21f52fc273de8.mailgun.org>',
                 to: "dave@southcitychurch.org;info@southcitychurch.org;$email.to",
